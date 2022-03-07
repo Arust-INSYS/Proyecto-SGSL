25cf3df51096ad3f43090c60f91a9f532ad0c92b
Arust-INSYS <90620456+Arust-INSYS@users.noreply.github.com>
2022-03-07 18:16:20 -0500

    Cambio de versión Java
    Se cambio a la versión de java 8

--- Sistema-SGSL/src/Ejecutar.java
+++ Sistema-SGSL/src/Ejecutar.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication23;
 
 /**
  *
  * @author Arust
  */
-public class JavaApplication23 {
+public class Ejecutar {
 
     /**
      * @param args the command line arguments
